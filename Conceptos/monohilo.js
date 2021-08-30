5390e5c6e6c75b605606928989bf3915391915da
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-30 14:07:44 -0500

    Execution of all instructions

--- Conceptos/monohilo.js
+++ Conceptos/monohilo.js
@@ -5,8 +5,10 @@ setInterval(function() {
     console.log(i);
     i++;
     
-    if(i === 5) {
-        // console.log('forzamos error');
-        var a = 3 + z;
-    }
+    // if(i === 5) {
+    //     // console.log('forzamos error');
+    //     var a = 3 + z;
+    // }
 }, 1000)
+
+console.log("Segunda Instrucción");
