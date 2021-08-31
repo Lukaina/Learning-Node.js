4b5fe0365f49507bfcd0319ab527e7072c2301d6
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-31 14:25:14 -0500

    More than one callback

--- Async/callback.js
+++ Async/callback.js
@@ -5,7 +5,16 @@ function saludo(nombre, myCallback) {
     }, 1000)
 }
 
+function despedida(nombre, otherCallback) {
+    setTimeout(function(){
+        console.log('Adiós ' + nombre)
+        otherCallback()
+    }, 1000)
+}
+
 console.log('Iniciando proceso...')
 saludo('Vero', function(){
-    console.log('Terminando proceso...')
+    despedida('Vero', function(){
+        console.log('Terminando proceso...')
+    })
 })
